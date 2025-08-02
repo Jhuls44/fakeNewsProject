6e808e0195210c0a483c1dbba97b421e38ff734c
Jhulians Garcia Hinojosa <garciajhulian@gmail.com>
2025-08-02 18:13:54 -0400

    Implementacion del header y su style

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  currentDate: Date = new Date();
 
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentDate = new Date();
+    }, 60000);
+  }
 }
