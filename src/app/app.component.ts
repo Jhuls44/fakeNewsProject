eeec51af7235412239209188d7a4cacabd90f2cc
Jhulians Garcia Hinojosa <garciajhulian@gmail.com>
2025-08-02 19:18:32 -0400

    implementacion de espacio donde se ven las noticias ya verificadas como falsas y implementacion del style.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NewsSectionComponent } from './components/news-section/news-section.component';
 import { NewsVerifierComponent } from "./components/news-verifier/news-verifier.component";
+import { VerifiedNewsComponent } from './components/verified-news/verified-news.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,9 @@ import { NewsVerifierComponent } from "./components/news-verifier/news-verifier.
   imports: [RouterOutlet,
     HeaderComponent,
     FooterComponent,
-    NewsSectionComponent, NewsVerifierComponent],
+    NewsSectionComponent, 
+    NewsVerifierComponent,
+    VerifiedNewsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'] 
 })
